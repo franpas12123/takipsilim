e07549d447e7f0036d9acbc81f5943a6d32d51fa
Frances D Pasulohan <franpas12123@gmail.com>
2021-01-20 10:56:09 +0800

    Added state machine for player movement. Added Dirt and Cliff Dirt Tile Map

--- Player/Player.cs
+++ Player/Player.cs
@@ -3,6 +3,13 @@ using Godot;
 public class Player : KinematicBody2D
 {
     Vector2 velocity;
+    const int ACCELERATION = 400;
+    const int MAX_SPEED = 80;
+    const int FRICTION = 500;
+
+    AnimationTree animationTree;
+    AnimationNodeStateMachinePlayback animationState;
+
     // Declare member variables here. Examples:
     // private int a = 2;
     // private string b = "text";
@@ -11,6 +18,8 @@ public class Player : KinematicBody2D
     public override void _Ready()
     {
         velocity = Vector2.Zero;
+        animationTree = GetNode<AnimationTree>("AnimationTree");
+        animationTree.Active = true;
     }
 
     //  // Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -21,19 +30,28 @@ public class Player : KinematicBody2D
     public override void _PhysicsProcess(float delta)
     {
         Vector2 inputVector = Vector2.Zero;
+        animationState = (AnimationNodeStateMachinePlayback)animationTree.Get("parameters/playback");
 
         inputVector.x = Input.GetActionStrength("ui_right") - Input.GetActionStrength("ui_left");
         inputVector.y = Input.GetActionStrength("ui_down") - Input.GetActionStrength("ui_up");
+        inputVector = inputVector.Normalized();
 
         if (inputVector != Vector2.Zero)
         {
-            velocity = inputVector;
+            // Set animation tree blend position values based on inputVector value
+            animationTree.Set("parameters/Idle/blend_position", inputVector);
+            animationTree.Set("parameters/Run/blend_position", inputVector);
+
+            animationState.Travel("Run");
+            velocity = velocity.MoveToward(inputVector * MAX_SPEED, ACCELERATION * delta);
         }
         else
         {
-            velocity = Vector2.Zero;
+            animationState.Travel("Idle");
+            // Move
+            velocity = velocity.MoveToward(Vector2.Zero, FRICTION * delta);
         }
 
-        MoveAndCollide(velocity);
+        velocity = MoveAndSlide(velocity);
     }
 }
